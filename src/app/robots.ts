54d1258458aaf04faa464a50c57e15bf8f660668
Darnley Weekes <darnley.weekes@gmail.com>
2026-09-11 15:06:37 -0700

    Fix production sitemap URL in robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://pitchme.example.com/sitemap.xml",
+    sitemap: "https://pitchme.prime24ai.com/sitemap.xml",
   };
 }
